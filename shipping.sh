4af8aeb2664409b4443d1780949c9889882d985f
shaikmadeen <shaikmadeen29@gmail.com>
2024-06-02 16:57:41 +0530

    shipping is added

--- shipping.sh
+++ shipping.sh
@@ -63,7 +63,7 @@ mv target/shipping-1.0.jar shipping.jar &>> $LOGFILE
 
 VALIDATE $? "renaming jar file"
 
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
+cp /home/centos/roboshop/shipping.service /etc/systemd/system/shipping.service &>> $LOGFILE
 
 VALIDATE $? "copying shipping service"
 
