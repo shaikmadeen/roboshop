0d8ee1042cdcb65c0f710be0836173c5b71e72a2
shaikmadeen <163907894+shaikmadeen@users.noreply.github.com>
2024-06-08 02:08:32 +0530

    shell

--- create-ec2.sh
+++ create-ec2.sh
@@ -2,7 +2,7 @@
 
 AMI=ami-0b4f379183e5706b9 #this keeps on changing
 SG_ID=sg-011403b343fdfa7c9 #replace with your SG ID
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" #"payment" "dispatch" "web")
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping") #"payment" "dispatch" "web"
 ZONE_ID=Z04800941MANNORN4ZXRP # replace your zone ID
 DOMAIN_NAME="devops.online"
 
